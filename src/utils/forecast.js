e06be4d7ba5854a14b59de83207772866cfff0ec
jellevw <jelle.vanwijhe@gmail.com>
2022-08-16 17:58:36 +0200

    Add windspeed to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('unable to find location', undefined)
     } else {
-      callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out')
+      callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' + '\n\n Windspeed: ' + body.current.wind_speed)
     }
   })
 }
